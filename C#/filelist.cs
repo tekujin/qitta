c7495b06c9769ca2bc52c42ce33344067c53b4c6
DESKTOP-BGC56J9\R2D2 <xiger78@gmail.com>
2022-10-16 11:42:04 +0900

    no message

---
+++ C#/filelist.cs
@@ -0,0 +1,54 @@
+using System;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace filelist
+{
+    class Program
+    {
+        static readonly HashAlgorithm hashProvider = new SHA1CryptoServiceProvider();
+
+        /// <summary>
+        /// Returns the hash string for the file.
+        /// </summary>
+        /// <param name="filePath"></param>
+        /// <returns></returns>
+        public static string ComputeFileHash(string filePath)
+        {
+            var fs = new FileStream(filePath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
+            var bs = hashProvider.ComputeHash(fs);
+            return BitConverter.ToString(bs).ToLower().Replace("-", "");
+        }
+
+        static void Main(string[] args)
+        {
+            string path1 = @".\";
+            try
+            {
+                var filename = Directory.EnumerateFiles(path1, "*.*", System.IO.SearchOption.AllDirectories);
+
+                StreamWriter sw = new StreamWriter(@".\file" + DateTime.Now.ToString("yyyyMMddHHmmss") + ".txt");
+
+                foreach (string f in filename)
+                {
+                    if (args.Length == 1 && args[0].ToString().ToUpper() == "-H")
+                    {
+                        Console.WriteLine(ComputeFileHash(f) + " " + f.Substring(2,f.Length - 2));
+                        sw.WriteLine(ComputeFileHash(f) + " " + f.Substring(2, f.Length - 2));
+                    }
+                    else
+                    {
+                        Console.WriteLine(f.Substring(2, f.Length - 2));
+                        sw.WriteLine(f.Substring(2, f.Length - 2));
+                    }
+                }
+
+                sw.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
